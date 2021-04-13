142a54c129d5fe9f73aec99fa21a78b0d8fbbd88
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-04-13 03:29:34 -0500

    update eng.js w/const from employee and extension w super for eng

--- Lib/engineer.js
+++ Lib/engineer.js
@@ -1,1 +1,12 @@
-engineer.js
+// engineer.js
+const employee = require("./employee");
+
+class engineer extends employee{
+    constructor(name, id, email, githubRepo, jobTitle) {
+        super(name, id, email);
+        this.githubRepo = githubRepo;
+        this.jobTitle;
+        
+    }
+
+}
