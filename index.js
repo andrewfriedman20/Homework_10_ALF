922ec73884641468f3f663016a1f439699ca93df
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-04-13 03:48:12 -0500

    major update to index.js inq inputs/ arrayPrompts

--- index.js
+++ index.js
@@ -4,6 +4,35 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 const engineer = require("./lib/engineer");
+
 const employee = require("./lib/employee");
+
 const intern = require("./lib/intern");
+
 const manager = require("./lib/manager");
+
+function runInquirer(){
+    const arrayPrompts = [{
+        type: 'input',
+        message: "What's your name?",
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: "Please enter your user ID.",
+        name: 'userID'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email address.',
+        name: 'email'
+    },
+    {
+        type:'list',
+        message: 'What is your job title?',
+        name: 'jobTitle'
+    }];
+
+    return inquirer.prompt(arrayPrompts);
+
+}
