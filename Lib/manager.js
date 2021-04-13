3fceb8d0231fa6511147ca9f1f98ef1866d04c41
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-04-13 03:34:00 -0500

    update mngr js w const emp & mnger class/ext/super

--- Lib/manager.js
+++ Lib/manager.js
@@ -1,1 +1,13 @@
-manager.js
+// manager.js
+
+const employee = require("./employee");
+
+class manager extends manager{
+    constructor(name, id, email, officeNo, jobTitle) {
+        super(name, id, email);
+        this.officeNo = officeNo;
+        this.jobTitle;
+        
+    }
+
+}
