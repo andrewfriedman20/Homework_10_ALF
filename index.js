9997f724f072c97e2f9edeb24fd53d4e6112f896
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-04-13 03:15:04 -0500

    establish consts in index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-index.js
+// index.js
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const engineer = require("./lib/engineer");
+const employee = require("./lib/employee");
+const intern = require("./lib/intern");
+const manager = require("./lib/manager");
