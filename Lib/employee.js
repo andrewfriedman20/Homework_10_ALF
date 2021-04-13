8ee1991f1fff7284eb9666b58539f32b0197e45e
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-04-13 03:28:34 -0500

    update employee js constructor/clss

--- Lib/employee.js
+++ Lib/employee.js
@@ -1,1 +1,12 @@
-employee.js
+// employee.js
+
+class employee {
+    constructor(name, id, email, jobTitle) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.jobTitle = jobTitle;
+
+    }
+
+}
