859289c7089da6b84a8a0fc2e6d723cbd151a899
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-04-15 17:51:22 -0500

    major update to mngr test code

--- test/managerTest.js
+++ test/managerTest.js
@@ -1,1 +1,26 @@
-managerTest js
+// managerTest.js
+
+const employee=require("./lib/employee");
+const manager=require("./lib/manager");
+
+
+test("May set office number using constructor argument", () => {
+    const testValue = 100;
+    const e = new manager("Jasper", 1, testValue);
+    expect(e.officeNo).tobe(testValue);
+
+  });
+  
+test("getRole() returns 'manager'", () => {
+    const testValue = "manager";
+    const e = new manager("Jasper", 1, "test@gmail.com", 100);
+    expect(e.getJob()).tobe(testValue);
+
+  });
+
+  test("May get office number using getofficeNo()", () => {
+    const testValue = 100;
+    const e = new manager("Jasper", 1, "test@gmail.com", testValue);
+    expect(e.officeNo()).tobe(testValue);
+
+  });
