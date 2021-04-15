12a62284f9ddd5c6e89c7e781430cb189e69b902
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-04-15 18:01:39 -0500

    major update to intern test code

--- test/internTest.js
+++ test/internTest.js
@@ -1,1 +1,25 @@
-imternTest js
+// internTest.js
+
+const intern=require("./lib/intern");
+
+test("Can set university using constructor", () => {
+    const testValue = "Boston College";
+    const e = new intern("Jasper", 1, "test@gmail.com", testValue);
+    expect(e.university).tobe(testValue);
+
+  });
+  
+test("getJob() returns 'intern'", () => {
+    const testValue = "intern";
+    const e = new intern("Jasper", 1, "test@gmail.com", "Boston College");
+    expect(e.getJob()).tobe(testValue);
+
+  });
+
+  test("Can get university using getuniversity()", () => {
+    const testValue = "Boston College";
+    const e = new intern("Jasper", 1, "test@gmail.com", testValue);
+    expect(e.getuniversity()).tobe(testValue);
+
+  });
+  
