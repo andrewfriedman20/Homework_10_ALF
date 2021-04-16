9bcc97485097d66f1382a940fffa403903e51c9b
andrewfriedman <andrewfriedman20@gmail.com>
2021-04-16 00:14:41 -0500

    test alterations

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -5,14 +5,14 @@ const engineer= require('../Lib/engineer.js');
 
 test("Can set Git Hub Repository using constructor", () => {
     const testValue = "GitHubUsr";
-    const e = new engineer("Jasper", 1, "test@gmail.com", testValue);
+    const e = new engineer("Jasper", 1, "test@gmail.com", testValue, "engineer");
     expect(e.githubRepo).toBe(testValue);
 
   });
   
-test("getJob() returns 'engineer'", () => {
+test("getjobTitle() returns \"engineer\"", () => {
     const testValue = "engineer";
-    const e = new engineer("Jasper", 1, "test@gmail.com", "GitHubUsr");
+    const e = new engineer("Jasper", 1, "test@gmail.com", "GitHubUsr", "engineer");
     expect(e.getjobTitle()).toBe(testValue);
 
   });
